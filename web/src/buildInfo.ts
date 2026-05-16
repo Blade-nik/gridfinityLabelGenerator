32f7327c9d4ca6a274bc1c6d5c7d9a17a2a2b6ee
Valentin Zaicev <zaicev@marvel.loc>
2026-05-16 14:24:49 +0300

    Fix icons and update dependencies

--- web/src/buildInfo.ts
+++ web/src/buildInfo.ts
@@ -1,2 +1,2 @@
-export const BUILD_TIME_ISO = "2026-05-16T11:07:48.241Z";
-export const BUILD_ID = "1778929668242";
+export const BUILD_TIME_ISO = "2026-05-16T11:23:42.780Z";
+export const BUILD_ID = "1778930622781";
